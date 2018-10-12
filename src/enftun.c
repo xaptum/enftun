6ac7129c24a4855dbf1f6dc9d069d14b22207d1f
David R. Bild <david.bild@xaptum.com>
2018-10-12 15:15:35 +0000

    context: refactor into separate file
    Keep the context struct and methods separate from the main program
    loop.

--- src/enftun.c
+++ src/enftun.c
@@ -17,107 +17,13 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-#include "cert.h"
 #include "channel.h"
-#include "enftun.h"
+#include "context.h"
 #include "filter.h"
 #include "log.h"
 
-int
-enftun_context_init(struct enftun_context* ctx)
-{
-    int rc;
-
-    rc = enftun_options_init(&ctx->options);
-    if (rc < 0)
-        goto err;
-
-    rc = enftun_tls_init(&ctx->tls);
-    if (rc < 0)
-        goto free_options;
-
-    rc = enftun_tun_init(&ctx->tun);
-    if (rc < 0)
-        goto free_tls;
-
-    rc = uv_loop_init(&ctx->loop);
-    if (rc < 0)
-        goto free_tun;
-
-    memset(ctx->ipv6_str, 0, sizeof(ctx->ipv6_str));
-
-    return 0;
-
- free_tun:
-    enftun_tun_free(&ctx->tun);
-
- free_tls:
-    enftun_tls_free(&ctx->tls);
-
- free_options:
-    enftun_options_free(&ctx->options);
-
- err:
-    return rc;
-}
-
-int
-enftun_context_free(struct enftun_context* ctx)
-{
-    uv_loop_close(&ctx->loop);
-
-    enftun_tun_free(&ctx->tun);
-    enftun_tls_free(&ctx->tls);
-
-    enftun_options_free(&ctx->options);
-
-    return 0;
-}
-
-/**
- * Adds missing colons back into IPv6 string.
- */
-static
-void
-fix_ipv6_string(char *ipv6_str)
-{
-    int i, c;
-    for (i = 28, c = 7; i >= 0; i = i - 4, c = c - 1)
-    {
-        memcpy(ipv6_str + i + c, ipv6_str + i, 4);
-        if (i > 0)
-            *(ipv6_str + i + c - 1) = ':';
-    }
-}
-
-int
-enftun_context_ipv6_from_cert(struct enftun_context* ctx, const char* file)
-{
-    int rc;
-
-    if ((rc = enftun_cert_common_name_file(ctx->options.cert_file,
-                                           ctx->ipv6_str,
-                                           sizeof(ctx->ipv6_str))) < 0)
-    {
-        enftun_log_error("Failed to read IPv6 address from cert %s\n", file);
-        goto out;
-    }
-
-    fix_ipv6_string(ctx->ipv6_str);
-
-    if (inet_pton(AF_INET6, ctx->ipv6_str, &ctx->ipv6) != 1)
-    {
-        enftun_log_error("Invalid IPv6 address (%s) in cert %s\n", ctx->ipv6_str);
-        rc = -1;
-        goto out;
-    }
-
- out:
-    return rc;
-}
 
 static void chain_complete(struct enftun_chain* chain, int status);
 
