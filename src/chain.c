xaptum/enftun

commit 8d73ebab1369d2d2c73ea1db0bdc39c1a50028a9

chain: fix accidental fallthrough in switch

src/chain.c
@@ -38,10 +38,10 @@ on_complete(struct enftun_crb* crb)
         handled = chain->filter(chain, &chain->packet);
         if (!handled)
         {
-        chain->state = enftun_chain_writing;
-        enftun_crb_write(&chain->crb, chain->output);
-        break;
+            chain->state = enftun_chain_writing;
+            enftun_crb_write(&chain->crb, chain->output);
         }
+        break;
     case enftun_chain_writing:
         chain->state = enftun_chain_reading;
         enftun_crb_read(&chain->crb, chain->input);

commit 796b76551f2eb9a46bc20fd940c86e4bc9474e56

chain: continuing reading if filter handled prior packet
The logic for alternating between reads and writes in a chain was
rather confusing and involved a "switch" statement fallthrough. This
fallthrough appeared incorrect and was removed in an earlier commit,
breaking the logic.

This commit refactors the logic to be explicit and restore correct
behavior.

Fixes: 8d73ebab1369 ("chain: fix accidental fallthrough in switch")

src/chain.c
@@ -21,6 +21,22 @@
 
 static
 void
+do_read(struct enftun_chain* chain)
+{
+    chain->state = enftun_chain_reading;
+    enftun_crb_read(&chain->crb, chain->input);
+}
+
+static
+void
+do_write(struct enftun_chain* chain)
+{
+    chain->state = enftun_chain_writing;
+    enftun_crb_write(&chain->crb, chain->output);
+}
+
+static
+void
 on_complete(struct enftun_crb* crb)
 {
     struct enftun_chain* chain = (struct enftun_chain*) crb->context;
@@ -36,15 +52,13 @@ on_complete(struct enftun_crb* crb)
     {
     case enftun_chain_reading:
         handled = chain->filter(chain, &chain->packet);
-        if (!handled)
-        {
-            chain->state = enftun_chain_writing;
-            enftun_crb_write(&chain->crb, chain->output);
-        }
+        if (handled)
+            do_read(chain);
+        else
+            do_write(chain);
         break;
     case enftun_chain_writing:
-        chain->state = enftun_chain_reading;
-        enftun_crb_read(&chain->crb, chain->input);
+        do_read(chain);
         break;
     }
 }
@@ -80,8 +94,7 @@ enftun_chain_start(struct enftun_chain* chain,
                    enftun_chain_complete complete)
 {
     chain->complete = complete;
-    chain->state = enftun_chain_reading;
-    enftun_crb_read(&chain->crb, chain->input);
+    do_read(chain);
     return 0;
 }
 